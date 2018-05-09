00ac6ca6e63b42333a4a8d3d663e710f988f3732
Dmitry Malyukov <dmitry.malyukov@salonultimate.com>
2018-05-08 22:14:29 -0400

    Added Formatter from input values to DTO and tests for its

---
+++ RSSAnalyzing.Tests/RawToDTOFormatterTests.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using RSSAnalyzing;
+using System.Threading.Tasks;
+using System.Net;
+using System.Linq;
+using System.IO;
+
+namespace RSSAnalyzing.Tests
+{
+    public class RawToDTOFormatterTests
+    {
+		#region CompaniesURIs input
+
+		[Fact]
+		public void CompaniesURIs_EmptyCompanyNameThrowsException()
+		{
+			// Arrange
+			var inputValues = new Dictionary<string, Uri>()
+			{
+				{ "FirstCompany", new Uri("https://www.example1.com") },
+				{ "SecondCompany", new Uri("https://www.example2.com") },
+				{ "", new Uri("https://www.example3.com") }
+			};
+
+			// Act
+			Func<Task> act = () => RawToDTOFormatter.Format(inputValues);
+
+			// Assert
+			Assert.ThrowsAsync<ArgumentException>(act);
+		}
+
+		[Fact]
+		public void CompaniesURIs_EmptyURIThrowsException()
+		{
+			// Arrange 
+			var inputValues = new Dictionary<string, Uri>()
+			{
+				{ "FirstCompany", new Uri("https://www.example1.com") },
+				{ "SecondCompany", null }
+			};
+
+			// Act
+			Func<Task> act = () => RawToDTOFormatter.Format(inputValues);
+
+			// Assert
+			Assert.ThrowsAsync<ArgumentException>(act);
+		}
+
+		[Fact]
+		public void CompaniesURIs_NotExistingUri()
+		{
+			// Arrange
+			var inputValues = new Dictionary<string, Uri>()
+			{
+				{ "FirstCompany", new Uri("https://www.dropbox.com/s/4rwsu8goeq7zx1h/bbc_world.xml?dl=1") },
+				{ "SecondCompany", new Uri("http://example.com/rss.xml") },
+			};
+
+			// Act
+			Func<Task> act = () => RawToDTOFormatter.Format(inputValues);
+
+			// Assert
+			Assert.ThrowsAsync<WebException>(act);
+		}
+
+		[Fact]
+		public async Task CompaniesURIs_UsualHandling()
+		{
+			// Arrange
+			var inputValues = new Dictionary<string, Uri>()
+			{
+				{ "FirstCompany", new Uri("https://www.dropbox.com/s/4rwsu8goeq7zx1h/bbc_world.xml?dl=1") },
+				{ "SecondCompany", new Uri("https://www.dropbox.com/s/acypcml3d89v38i/bbc_world_addition.xml?dl=1") },
+			};
+
+			// Act
+			var result = await RawToDTOFormatter.Format(inputValues);
+
+			// Assert
+			var firstCompanyFeedCount = result.First(x => x.Company == "FirstCompany").Feed.Count();
+			var secondCompanyFeedCount = result.First(x => x.Company == "SecondCompany").Feed.Count();
+			Assert.True(firstCompanyFeedCount == 4 && secondCompanyFeedCount == 11);
+		}
+
+		#endregion
+
+		#region CompaniesXMLs_input
+		[Fact]
+		public void CompaniesXMLs_EmptyCompanyNameThrowsException()
+		{
+			// Arrange 
+			var inputValues = new Dictionary<string, string>()
+			{
+				{ "FirstCompany", "xml" },
+				{ "", "xml" }
+			};
+
+			// Act
+			Func<Task> act = () => RawToDTOFormatter.Format(inputValues);
+
+			// Assert
+			Assert.ThrowsAsync<ArgumentException>(act);
+		}
+
+		[Fact]
+		public void CompaniesXMLs_EmptyXMLThrowsException()
+		{
+			// Arrange 
+			var inputValues = new Dictionary<string, string>()
+			{
+				{ "FirstCompany", "xml" },
+				{ "SecondCompany", "" }
+			};
+
+			// Act
+			Func<Task> act = () => RawToDTOFormatter.Format(inputValues);
+
+			// Assert
+			Assert.ThrowsAsync<ArgumentException>(act);
+		}
+
+		[Fact]
+		public async Task CompaniesXMLs_UsualHandling()
+		{
+			// Arrange 
+			var exampleXmlNotOlder2Days = File.ReadAllText("RssExample_NotOlder2Days.xml");
+			var exampleXmlOlder2Days = File.ReadAllText("RssExample_Older2Days.xml");
+			var inputValues = new Dictionary<string, string>()
+			{
+				{ "FirstCompany", exampleXmlNotOlder2Days },
+				{ "SecondCompany", exampleXmlOlder2Days }
+			};
+
+			// Act
+			var result = await RawToDTOFormatter.Format(inputValues);
+
+			// Assert
+			var itemsCountNotOlder = result.First(x => x.Company == "FirstCompany").Feed.Count();
+			var itemsCountOlder = result.First(x => x.Company == "SecondCompany").Feed.Count();
+			Assert.True(itemsCountNotOlder == 2 && itemsCountOlder == 2);
+		}
+		#endregion
+	}
+}
---
+++ RSSAnalyzing/RawToDTOFormatter.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.SyndicationFeed;
+using Microsoft.SyndicationFeed.Rss;
+using RSSAnalyzing.DTOs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace RSSAnalyzing
+{
+    public class RawToDTOFormatter
+	{
+		/// <summary>
+		///		Builds list of CompanyFeedDTO records based <paramref name="companiesFeedsRaw"/>
+		/// </summary>
+		/// <param name="companiesFeedsRaw"> dictionary of CompanyName and RSS URI </param>
+		/// <returns> list of CompanyFeedDTO records </returns>
+		public static async Task<IEnumerable<CompanyFeedDTO>> Format(Dictionary<string, Uri> companiesFeedsRaw)
+		{
+			var result = new List<CompanyFeedDTO>();
+
+			if (companiesFeedsRaw.Keys.Any(x => string.IsNullOrEmpty(x)))
+				throw new ArgumentException("At least one Company name is empty");
+
+			if (companiesFeedsRaw.Values.Any(x => x == null))
+				throw new ArgumentException("At least one RSS URI is null");
+
+			foreach (var rawItem in companiesFeedsRaw)
+			{
+				var company = rawItem.Key;
+				var feedUri = rawItem.Value;
+
+				using (var xmlReader = XmlReader.Create(feedUri.AbsoluteUri))
+				{
+					var feed = await GetRSSItems(xmlReader);
+					result.Add(new CompanyFeedDTO(company, feed));
+				}
+			}
+
+			return result;
+		}
+
+		/// <summary>
+		///		Builds list of CompanyFeedDTO records based <paramref name="companiesFeedsRaw"/>
+		/// </summary>
+		/// <param name="companiesFeedsRaw"> dictionary of CompanyName and RSS XML as string </param>
+		/// <returns> list of CompanyFeedDTO records </returns>
+		public static async Task<IEnumerable<CompanyFeedDTO>> Format(Dictionary<string, string> companiesFeedsRaw)
+		{
+			var result = new List<CompanyFeedDTO>();
+
+			if (companiesFeedsRaw.Keys.Any(x => string.IsNullOrEmpty(x)))
+				throw new ArgumentException("At least one Company name is empty");
+
+			if (companiesFeedsRaw.Values.Any(x => string.IsNullOrEmpty(x)))
+				throw new ArgumentException("At least one feed XML is null");
+
+			foreach (var rawItem in companiesFeedsRaw)
+			{
+				var company = rawItem.Key;
+				var feedXml = rawItem.Value;
+
+				using (var textReader = new StringReader(feedXml))
+				{
+					using (var xmlReader = XmlReader.Create(textReader))
+					{
+						var feed = await GetRSSItems(xmlReader);
+						result.Add(new CompanyFeedDTO(company, feed));
+					}
+				}
+			}
+
+			return result;
+		}
+
+		/// <summary>
+		///		Gets all RSS items from <paramref name="xmlReader"/>
+		/// </summary>
+		/// <param name="xmlReader"> xml feed reader </param>
+		/// <returns> list of RSS items </returns>
+		private static async Task<IEnumerable<ISyndicationItem>> GetRSSItems(XmlReader xmlReader)
+		{
+			var result = new List<ISyndicationItem>();
+
+			var feedReader = new RssFeedReader(xmlReader);
+			while (await feedReader.Read())
+			{
+				if (feedReader.ElementType == SyndicationElementType.Item)
+					result.Add(await feedReader.ReadItem());
+			}
+
+			return result;
+		}
+	}
+}
