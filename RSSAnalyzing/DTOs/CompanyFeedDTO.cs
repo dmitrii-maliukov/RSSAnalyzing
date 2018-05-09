f76fb80300abdcdebd333c7c158ff7b29cb766b0
Dmitry Malyukov <dmitry.malyukov@salonultimate.com>
2018-05-08 20:08:31 -0400

    Added DTO which will be handling then

---
+++ RSSAnalyzing/DTOs/CompanyFeedDTO.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.SyndicationFeed;
+using System.Collections.Generic;
+
+namespace RSSAnalyzing.DTOs
+{
+	public class CompanyFeedDTO
+	{
+		public string Company { get; private set; }
+		public IEnumerable<ISyndicationItem> Feed { get; private set; }
+
+		public CompanyFeedDTO(string company, IEnumerable<ISyndicationItem> feed)
+		{
+			Company = company;
+			Feed = feed;
+		}
+	}
+}
